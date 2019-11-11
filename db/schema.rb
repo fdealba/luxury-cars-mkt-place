ab8e4f26f897988a1432dc77fa74cc81830452d8
bitna kim <bitna153@gmail.com>
2019-11-11 17:28:37 +0100

    nothing

--- db/schema.rb
+++ db/schema.rb
@@ -23,9 +23,9 @@ ActiveRecord::Schema.define(version: 2019_11_11_144051) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "phone_numer", null: false
-    t.string "full_name", null: false
-    t.string "role", null: false
+    t.string "phone_numer"
+    t.string "full_name"
+    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
