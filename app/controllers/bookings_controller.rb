dd141a6ccb2174acedc2bc2dd392bf3d7cd5b66f
Fanny Rojon <fanny.rojon22@gmail.com>
2019-11-12 11:44:26 +0100

    edited the show page for user to see a specific car

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,24 @@
 class BookingsController < ApplicationController
   def new
+    @car = Car.find(params[:car_id])
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @car = Car.find(params[:car_id])
+    @booking.car = @car
+    @booking.save
+    redirect_to car_path(@car)
   end
 
   def index
   end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:renting_time)
+  end
 end
+
