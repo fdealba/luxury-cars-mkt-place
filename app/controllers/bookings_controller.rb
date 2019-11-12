bd79275cbfc6eedda7c3fbfe47dd0ddc574580da
bitna kim <bitna153@gmail.com>
2019-11-12 11:57:34 +0100

    new booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,11 +1,23 @@
 class BookingsController < ApplicationController
   def new
     @booking = Bookng.new
+    #form for booking HTML
+    # this form can be in the car show page
   end
 
   def create
+    @car = Car.find(params[:car_id])
+    @booking = Booking.new(booking_params)
+    # @booking.car = @car
+    @car.booking = @booking.car
+    @current_user = @booking.user
+    @booking.save
+    redirect_to car_path(@car)
   end
 
-  def index
+  private
+
+  def booking_params
+    params.require(:booking).permit(:renting_time, :status))
   end
 end
