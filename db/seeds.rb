f2887a3b02fade81bffd0730550c8f01dfbf006b
Fanny Rojon <fanny.rojon22@gmail.com>
2019-11-12 16:28:53 +0100

    working on seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Car.destroy_all
+user1 = User.new
+car1 = Car.create!(
+  brand: "BMW",
+  car_model: "7 Series",
+  seats: 5,
+  plate_number: "3650GYG",
+  combustion: "Diesel",
+  milage: 50_000,
+  transmission: "automatic",
+  price_per_hour: 200,
+  price_per_day: 2_500,
+  location: "Madrid",
+  accessories: "Air Conditionning",
+  year: 2017,
+  user: user1
+  )
+
