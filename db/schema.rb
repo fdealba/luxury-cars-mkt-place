05365bcf0b632c640f9863b13815f128011f8e67
Fanny Rojon <fanny.rojon22@gmail.com>
2019-11-12 10:20:21 +0100

    created booking new method

--- db/schema.rb
+++ db/schema.rb
@@ -55,7 +55,7 @@ ActiveRecord::Schema.define(version: 2019_11_11_150820) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "phone_numer"
+    t.string "phone_number"
     t.string "full_name"
     t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
