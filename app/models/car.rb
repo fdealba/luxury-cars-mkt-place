2adbe8a2879d099cb5fcc15fa59d00ae69e08fc4
Karim Soussi <mrksoussi@gmail.com>
2019-11-13 12:41:38 +0100

    The calm before the merge

--- app/models/car.rb
+++ app/models/car.rb
@@ -5,6 +5,6 @@ class Car < ApplicationRecord
   validates :brand, presence: true
   validates :milage, presence: true
   validates :price_per_hour, presence: true
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,58 +18,11 @@ user2 = User.create!(
 user3 = User.create!(
   email: "bitna@lewagon.com",
   password: "123456")
-user4 = User.create!(
-  email: "fanny@lewagon.com",
-  password: "123456")
+user5 = User.create!(email: "test@lewagon.com", password: "123456")
 
-car1 = Car.create!(
-  brand: "BMW",
-  car_model: "7 Series",
-  seats: 5,
-  plate_number: "3650GYG",
-  combustion: "Diesel",
-  milage: 50_000,
-  transmission: "automatic",
-  price_per_hour: 200,
-  price_per_day: 2_500,
-  location: "Madrid",
-  accessories: "Air Conditionning",
-  year: 2017,
-  user: user1,
-  photo: "https://cdn2.rcstatic.com/images/car_images/web/bmw/7_series_lrg.jpg"
-  )
-car2 = Car.create!(
-  brand: "Mercedes-Benz",
-  car_model: "C-Class",
-  seats: 5,
-  plate_number: "3670PYG",
-  combustion: "Diesel",
-  milage: 50_000,
-  transmission: "automatic",
-  price_per_hour: 350,
-  price_per_day: 3_000,
-  location: "Barcelona",
-  accessories: "Air Conditionning",
-  year: 2017,
-  user: user2,
-  photo: "https://cdn2.rcstatic.com/images/car_images/web/mercedes/c_class_lrg.jpg"
-  )
-car3 = Car.create!(
-  brand: "Opel",
-  car_model: "Cascada Cabrio",
-  seats: 4,
-  plate_number: "4550GYG",
-  combustion: "Gas",
-  milage: 40_000,
-  transmission: "Manual",
-  price_per_hour: 180,
-  price_per_day: 2_000,
-  location: "Valencia",
-  accessories: "Audio / iPod input",
-  year: 2017,
-  user: user3,
-  photo: "https://cdn2.rcstatic.com/images/car_images/new_images/opel/cascada_lrg.jpg"
-  )
+car1 = Car.create!(brand: "BMW", car_model: "7 Series", seats: 5, plate_number: "3650GYG", combustion: "Diesel", milage: 50_000, transmission: "automatic", price_per_hour: 200, price_per_day: 2_500, location: "Madrid", accessories: "Air Conditionning", year: 2017, user: user5, photo: "https://cdn2.rcstatic.com/images/car_images/web/bmw/7_series_lrg.jpg", latitude: 40.420248, longitude: -3.677749)
+car2 = Car.create!(brand: "Mercedes-Benz", car_model: "C-Class", seats: 5,plate_number: "3670PYG",combustion: "Diesel",milage: 50_000,transmission: "automatic",price_per_hour: 350,price_per_day: 3_000,location: "Barcelona",accessories: "Air Conditionning",year: 2017,user: user6,photo: "https://cdn2.rcstatic.com/images/car_images/web/mercedes/c_class_lrg.jpg")
+car3 = Car.create!(brand: "Opel",car_model: "Cascada Cabrio",seats: 4,plate_number: "4550GYG",combustion: "Gas",milage: 40_000,transmission: "Manual",price_per_hour: 180,price_per_day: 2_000,location: "Valencia",accessories: "Audio / iPod input",year: 2017,user_id: 12,photo: "https://cdn2.rcstatic.com/images/car_images/new_images/opel/cascada_lrg.jpg")
 car4 = Car.create!(
   brand: "Fiat",
   car_model: "500",
