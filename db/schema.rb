15ffacc4531c7fb2ede0ae61c83642a5174930a3
Karim Soussi <mrksoussi@gmail.com>
2019-11-15 10:59:43 +0100

    Deleted image from car

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_14_175657) do
+ActiveRecord::Schema.define(version: 2019_11_15_095340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,7 +51,6 @@ ActiveRecord::Schema.define(version: 2019_11_14_175657) do
     t.float "latitude"
     t.float "longitude"
     t.string "body_type"
-    t.string "image"
     t.index ["user_id"], name: "index_cars_on_user_id"
   end
 
